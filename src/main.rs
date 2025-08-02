30266dbf191da0ff8fae9936434070ffd2e71357
amorris <amorris@amorris.cc>
2025-08-02 16:20:45 -0400

    gradient, starting ch.5

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
-use std::{fmt::format, ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign}};
-const IMAGE_WIDTH: i32 = 400;
-const IMAGE_HEIGHT: i32 = 400;
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+const IMAGE_WIDTH: usize = 400;
+const ASPECT_RATIO: f32 = 16.0 /9.0;
+const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as usize;
 
 fn main() {
+    assert!(IMAGE_HEIGHT > 1);
+
+    let focal_length = 1.;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32);
+    let camera_center = Point3::default();
+
+    let viewport_u = Vec3 {
+        x:viewport_width, y:0.0, z:0.0
+    };
+    let viewport_v = Vec3 {
+        x:0.0, y:-viewport_height, z:0.0
+    };
+
+    let pixel_delta_u = viewport_u / IMAGE_WIDTH as f32;
+    let pixel_delta_v = viewport_v / IMAGE_HEIGHT as f32;
+
+    let viewport_upper_left = camera_center - Vec3::new(0.0,0.0,focal_length) - viewport_u / 2. - viewport_v / 2.;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+
+    // Render
     println!("P3");
     println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
     println!("255");
 
     for j in 0..IMAGE_HEIGHT {
         for i in 0..IMAGE_WIDTH {
-            let red: f32 = i as f32 / (IMAGE_HEIGHT - 1) as f32;
-            let green: f32 = j as f32 / (IMAGE_WIDTH - 1) as f32;
-            let blue: f32 = 0.0;
+            let pixel_center = pixel00_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let ray = Ray::new(camera_center, ray_direction);
 
-            let color = Color::new(red, green, blue);
-            println!("{}", color.write_color());
+            let pixel_color = ray.color();
+            pixel_color.write_color();
         }
     }
+    //End Render
 }
 
 #[derive(Copy, Debug, Default, Clone, PartialEq)]
@@ -26,15 +50,40 @@ struct Vec3 {
     z: f32,
 }
 
+#[derive(Copy, Debug, Default, Clone, PartialEq)]
+struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+
+impl Ray {
+    fn new(origin: Point3, direction: Vec3) -> Self {
+        Ray { origin, direction }
+    }
+
+    fn at(&self, t: f32) -> Point3 {
+        self.origin + t * self.direction
+    }
+    fn color(self) -> Color {
+        let unit_direction = self.direction.unit_vector();
+        let a = 0.5*(unit_direction.y + 1.0);
+        (1.0-a)*Color::new(1.,1.,1.) + a*Color::new(0.5,0.7,1.0)
+
+
+    }
+}
+
 type Color = Vec3;
 impl Color {
-    fn write_color(&self)-> String{
+    fn write_color(&self){
         let red_out: i32 = (self.x * 255.0) as i32;
         let green_out: i32 = (self.y * 255.0) as i32;
         let blue_out: i32 = (self.z * 255.0) as i32;
-        format!("{red_out} {green_out} {blue_out}")
+        println!("{red_out} {green_out} {blue_out}");
     }
 }
+
+type Point3 = Vec3;
 
 impl Vec3 {
     fn new(x:f32,y:f32,z:f32) -> Self {
