d67b54effe75bd0dec1484086b1cdd0ab090988d
amorris <amorris@amorris.cc>
2025-08-02 15:29:53 -0400

    vector math and color

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::{fmt::format, ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign}};
 const IMAGE_WIDTH: i32 = 400;
 const IMAGE_HEIGHT: i32 = 400;
 
 fn main() {
     println!("P3");
-    println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
+    println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
     println!("255");
 
     for j in 0..IMAGE_HEIGHT {
@@ -12,11 +13,8 @@ fn main() {
             let green: f32 = j as f32 / (IMAGE_WIDTH - 1) as f32;
             let blue: f32 = 0.0;
 
-            let red_out: i32 = (red * 255.0) as i32;
-            let green_out: i32 = (green * 255.0) as i32;
-            let blue_out: i32 = (blue * 255.0) as i32;
-
-            println!("{} {} {}", red_out, green_out, blue_out);
+            let color = Color::new(red, green, blue);
+            println!("{}", color.write_color());
         }
     }
 }
@@ -28,6 +26,143 @@ struct Vec3 {
     z: f32,
 }
 
+type Color = Vec3;
+impl Color {
+    fn write_color(&self)-> String{
+        let red_out: i32 = (self.x * 255.0) as i32;
+        let green_out: i32 = (self.y * 255.0) as i32;
+        let blue_out: i32 = (self.z * 255.0) as i32;
+        format!("{red_out} {green_out} {blue_out}")
+    }
+}
+
+impl Vec3 {
+    fn new(x:f32,y:f32,z:f32) -> Self {
+        Vec3 { x,  y,  z}
+    }
+    fn length_squared(&self) -> f32 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    fn length(&self) -> f32 {
+        self.length_squared().sqrt()
+    }
+    fn dot(&self, rhs: Self) -> f32 {
+        self.x * rhs.x +
+        self.y * rhs.y +
+        self.z * rhs.z
+    }
+
+    fn cross(&self, rhs: Self) -> Self {
+        Vec3{
+            x: self.y * rhs.z - self.z * self.y,
+            y: self.x * rhs.z - self.z * self.x,
+            z: self.y * rhs.x - self.x * self.y,
+        }
+    }
+    fn unit_vector(self) -> Self {
+        self / self.length()
+    }
+}
+
 impl Add for Vec3 {
     type Output = Self;
+    fn add(self, rhs: Self) -> Self {
+        Vec3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+    }
+}
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        self.x += rhs.x;
+        self.y += rhs.y;
+        self.z += rhs.z;
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self {
+        Vec3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+    }
+}
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        self.x -= rhs.x;
+        self.y -= rhs.y;
+        self.z -= rhs.z;
+    }
+}
+impl Mul for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self {
+        Vec3 { x: self.x * rhs.x, y: self.y * rhs.y, z: self.z * rhs.z }
+    }
+}
+impl MulAssign for Vec3 {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.x *= rhs.x;
+        self.y *= rhs.y;
+        self.z *= rhs.z;
+    }
+}
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self {
+        Vec3 { x: self.x * rhs, y: self.y * rhs, z: self.z * rhs }
+    }
+}
+impl MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output{
+        rhs * self
+    }
+}
+
+impl Div for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self {
+        Vec3 { x: self.x / rhs.x, y: self.y / rhs.y, z: self.z / rhs.z }
+    }
+}
+impl DivAssign for Vec3 {
+    fn div_assign(&mut self, rhs: Self) {
+        self.x /= rhs.x;
+        self.y /= rhs.y;
+        self.z /= rhs.z;
+    }
+}
+impl Div<f32> for Vec3 {
+    type Output = Self;
+    fn div(self, rhs: f32) -> Self {
+        Vec3 { x: self.x / rhs, y: self.y / rhs, z: self.z / rhs }
+    }
+}
+impl DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, rhs: f32) {
+        self.x /= rhs;
+        self.y /= rhs;
+        self.z /= rhs;
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+    fn neg(self) -> Self {
+        Vec3 { x: -self.x, y: -self.y, z: -self.z }
+    }
+}
+
+impl std::fmt::Display for Vec3 {
+    fn fmt(&self, f:&mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.x,self.y,self.z)
+    }
+
 }
